7ac4dff5a7bc14eab50aca2fc6dfb1205cdd19bf
Dennis Schmalstieg <darianbellentor@outlook.de>
2024-01-02 03:02:31 +0100

    first setup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'DA_10_1_Join_v3';
-}
+export class AppComponent {}
